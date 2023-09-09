9ec2f44aec0f9e859448aedcbaf756abad413f5e
Andrew Mo <106348010+theandrewmo@users.noreply.github.com>
2023-09-08 21:04:42 -0600

    added build

---
+++ jobly/build/precache-manifest.1ee357037b5abb9140e35d2af115c530.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4073f9c3298367c10a7a668c4e5b6710",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2f320ee8ec344716dde",
+    "url": "/static/css/main.08791a52.chunk.css"
+  },
+  {
+    "revision": "510ff5fb300c05bd8d60",
+    "url": "/static/js/2.8ebcd85a.chunk.js"
+  },
+  {
+    "revision": "3824c8cf61c071e1211a57c39259e2ca",
+    "url": "/static/js/2.8ebcd85a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2f320ee8ec344716dde",
+    "url": "/static/js/main.abd003a0.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.abd003a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e157b3318261263bb32a",
+    "url": "/static/js/runtime-main.ca09d11f.js"
+  }
+]);
